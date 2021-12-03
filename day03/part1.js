e4b0d7e04bc22cb12e6e67da7ec46a15a8f4812f
Sean Sartell <ssartell@rightpoint.com>
2021-12-03 14:28:11 -0600

    day03

--- day03/part1.js
+++ day03/part1.js
@@ -3,9 +3,10 @@ import R from 'ramda';
 const toDigit = R.pipe(R.join(''), x => parseInt(x, 2));
 const parseLine = R.pipe(R.trim(), R.split(''), R.map(parseInt));
 const parseInput = R.pipe(R.split('\n'), R.map(parseLine));
-const mostCommon = xs => Math.round(R.sum(xs) / xs.length);
+const mostCommon = xs => R.sum(xs) >= xs.length / 2;
+const leastCommon = xs => 1 - mostCommon(xs);
+
 const toGamma = R.pipe(R.map(mostCommon), toDigit);
-const leastCommon = xs => xs.length > 1 ? 1 - mostCommon(xs) : xs[0];
 const toEpsilon = R.pipe(R.map(leastCommon), toDigit);
 
 export default R.pipe(parseInput, R.transpose, R.converge(R.multiply, [toEpsilon, toGamma]));
