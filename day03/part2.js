8c83a0739bbb3437b473015889d673197c932dd6
Sean Sartell <ssartell@rightpoint.com>
2021-12-03 14:44:16 -0600

    day03

--- day03/part2.js
+++ day03/part2.js
@@ -5,9 +5,8 @@ const parseInput = R.pipe(R.split('\n'), R.map(parseLine));
 const mostCommon = xs => R.sum(xs) >= xs.length / 2 ? 1 : 0;
 const leastCommon = xs => xs.length > 1 ? 1 - mostCommon(xs) : xs[0];
 
-const getDigits = (xs, i) => R.pipe(R.transpose, xs => xs[i])(xs);
 const toDigit = R.pipe(R.join(''), x => parseInt(x, 2));
-const filterBy = R.curry((f, nums, i) => R.filter(x => x[i] === f(getDigits(nums, i)), nums));
+const filterBy = R.curry((f, nums, i) => R.filter(x => x[i] === f(R.transpose(nums)[i]), nums));
 const find = R.curry((f, nums) => toDigit(R.head(R.reduce(filterBy(f), nums, R.range(0, nums[0].length)))));
 
 export default R.pipe(parseInput, R.converge(R.multiply, [find(mostCommon), find(leastCommon)]));
