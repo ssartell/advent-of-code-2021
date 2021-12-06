fb4b52770ac10101cfee3931c1fd41bc818e4821
Sean Sartell <ssartell@rightpoint.com>
2021-12-06 11:02:46 -0600

    day05 cleanup

--- day05/part1.js
+++ day05/part1.js
@@ -1,20 +1,18 @@
 import R from 'ramda';
+import { add, sub, equals, toString, sign } from '../utils/vec2.js';
 
 const parsePair = R.pipe(R.split(','), R.map(parseInt), R.zipObj(['x', 'y']));
 const parseLine = R.pipe(R.split(' -> '), R.map(parsePair), R.zipObj(['p1', 'p2']));
 const isStraight = ({p1, p2}) => p1.x === p2.x || p1.y === p2.y;
 const parseInput = R.pipe(R.split('\n'), R.map(parseLine), R.filter(isStraight));
 
-const add = (p1, p2) => ({x: p1.x + p2.x, y: p1.y + p2.y});
-const equals = (p1, p2) => p1.x === p2.x && p1.y === p2.y;
-
 const findIntersections = lines => {
   let count = 0;
   const points = new Map();
   for(let line of lines) {
-    const dir = { x: Math.sign(line.p2.x - line.p1.x), y: Math.sign(line.p2.y - line.p1.y) };
+    const dir = sign(sub(line.p2, line.p1));
     for(let p = line.p1; !equals(p, add(line.p2, dir)); p = add(p, dir)) {
-      const pk = `${p.x},${p.y}`;
+      const pk = toString(p);
       points.set(pk, points.has(pk) ? points.get(pk) + 1 : 1);
       if (points.get(pk) === 2) count++;
     }
