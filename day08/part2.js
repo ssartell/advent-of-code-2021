fec48983341ef038bed8c5913cea10c150115098
Sean Sartell <ssartell@rightpoint.com>
2021-12-09 08:30:51 -0600

    switch to binary

--- day08/part2.js
+++ day08/part2.js
@@ -1,33 +1,33 @@
 import R from 'ramda';
-import M from 'mnemonist/set.js';
 
-const parseLine = R.pipe(R.split(' | '), R.map(R.split(' ')), R.map(R.map(x => new Set(x))));
+const toBinary = x => "abcdefg".split('').reduce((a, y) => (a << 1) | x.includes(y), 0);
+const parseLine = R.pipe(R.split(' | '), R.map(R.split(' ')), R.map(R.map(toBinary)));
 const parseInput = R.pipe(R.split('\r\n'), R.map(parseLine));
-
-let areSetsEqual = (a, b) => a.size === b.size && [...a].every(x => b.has(x));
+const size = x => (x.toString(2).match(/1/g) || []).length;
 
 const match = {
-  0: (map, x) => x.size === 6 && !match[6](map, x) && areSetsEqual(M.union(x, map[3]), map[8]),
-  1: (map, x) => x.size === 2,
-  2: (map, x) => x.size === 5 && !match[3](map, x) && areSetsEqual(M.union(x, map[4]), map[8]),
-  3: (map, x) => x.size === 5 && areSetsEqual(M.union(x, map[1]), x),
-  4: (map, x) => x.size === 4,
-  5: (map, x) => x.size === 5 && !match[3](map, x) && !match[2](map, x),
-  6: (map, x) => x.size === 6 && areSetsEqual(M.union(x, map[1]), map[8]),
-  7: (map, x) => x.size === 3,
-  8: (map, x) => x.size === 7,
-  9: (map, x) => x.size === 6 && !match[6](map, x) && !match[0](map, x)
+  0: (map, x) => size(x) === 6 && (x | map[3]) === map[8],
+  1: (map, x) => size(x) === 2,
+  2: (map, x) => size(x) === 5 && (x | map[4]) === map[8],
+  3: (map, x) => size(x) === 5 && (x | map[1]) === x,
+  4: (map, x) => size(x) === 4,
+  5: (map, x) => size(x) === 5,
+  6: (map, x) => size(x) === 6 && (x | map[1]) === map[8],
+  7: (map, x) => size(x) === 3,
+  8: (map, x) => size(x) === 7,
+  9: (map, x) => size(x) === 6
 };
 
 const solveOutput = ([signals, output]) => {
   let map = {};
-  for(let i of [1,7,4,8,3,2,5,6,0,9]){
+  for(let i of [1,7,4,8,3,2,5,6,0,9]) {
     map[i] = R.find(x => match[i](map, x), signals);
+    signals = R.without([map[i]], signals);
   }
   let res = "";
   for(let outputSet of output) {
     for(let i = 0; i <= 9; i++) {
-      if(areSetsEqual(map[i], outputSet)) {
+      if(map[i] === outputSet) {
         res += i;
       }
     }
