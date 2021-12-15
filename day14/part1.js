359b1fc2b86470e6bfd11d4725ba205d379a75c5
Sean Sartell <ssartell@rightpoint.com>
2021-12-14 22:34:19 -0600

    day14

--- day14/part1.js
+++ day14/part1.js
@@ -4,38 +4,23 @@ import { min, max } from '../utils/ramda.js';
 const parseRules = R.pipe(R.split('\r\n'), R.map(R.split(' -> ')), R.fromPairs);
 export const parseInput = R.pipe(R.split('\r\n\r\n'), R.zipObj(['template', 'rules']), R.evolve({ rules: parseRules }));
 
-const getCounts = str => {
-  const counts = {};
-  for (let i = 0; i < str.length; i++) {
-    const ch = str[i];
-    counts[ch] = (counts[ch] || 0) + 1;
+export const expandPolymer = R.memoizeWith(
+  (rules, str, depth) => str + depth, 
+  (rules, str, depth) => {
+    if (depth === 0) return R.countBy(R.identity, str);
+    let pairs = R.aperture(2, R.split('', str)).map(R.join(''));
+    let counts = R.reduce((counts, pair) => {
+      const rule = rules[pair];
+      const part1 = expandPolymer(rules, pair[0] + rule, depth - 1);
+      const part2 = expandPolymer(rules, rule + pair[1], depth - 1);
+      const subCounts = R.mergeWith(R.add, part1, part2);
+      subCounts[rule]--;
+      subCounts[pair[1]]--;     
+      return R.mergeWith(R.add, counts, subCounts);
+    }, {}, pairs);
+    counts[R.last(str)]++;
+    return counts;
   }
-  return counts;
-};
+);
 
-const key = (rules, str, depth) => str + depth;
-const run = R.memoizeWith(key, (rules, str, depth) => {
-  if (depth < 0) {
-    return getCounts(str);
-  }
-  const rule = rules[str];
-  const part1 = run(rules, str[0] + rule, depth - 1);
-  const part2 = run(rules, rule + str[1], depth - 1);
-  const counts = R.mergeWith(R.add, part1, part2);
-  counts[rule]--;
-  return counts;
-});
-
-export const split = (rules, template, depth) => {
-  let counts = {};
-  let pairs = R.aperture(2, R.split('', template));
-  for(let pair of pairs) {
-    counts = R.mergeWith(R.add, counts, run(rules, pair[0] + pair[1], depth - 1));
-    counts[pair[1]]--;
-  }
-  let lastChar = R.last(template);
-  counts[lastChar]++;
-  return counts;
-}
-
-export default R.pipe(parseInput, x => split(x.rules, x.template, 10), R.values, R.converge(R.subtract, [max, min]));
+export default R.pipe(parseInput, x => expandPolymer(x.rules, x.template, 10), R.values, R.converge(R.subtract, [max, min]));
--- day14/part2.js
+++ day14/part2.js
@@ -1,5 +1,5 @@
 import R from 'ramda';
 import { min, max } from '../utils/ramda.js';
-import { split, parseInput } from './part1.js';
+import { expandPolymer, parseInput } from './part1.js';
 
-export default R.pipe(parseInput, x => split(x.rules, x.template, 10), R.values, R.converge(R.subtract, [max, min]));
+export default R.pipe(parseInput, x => expandPolymer(x.rules, x.template, 40), R.values, R.converge(R.subtract, [max, min]));
