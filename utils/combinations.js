19b570c2f26558ab37e566132c806a65655ac6b8
Sean Sartell <ssartell@rightpoint.com>
2021-12-21 14:04:26 -0600

    day21

--- utils/combinations.js
+++ utils/combinations.js
@@ -1,20 +1,24 @@
-export default function* (list, n) {
-    var keys = [];
-    var index = 0;
-    for (var i = 0; i < n; i++) {
-      keys.push(-1);
-    }
-    while (index >= 0) {
-      if (keys[index] < list.length - (n - index)) {
-        for (var key = keys[index] - index + 1; index < n; index++) {
-          keys[index] = key + index;
-        }
-        yield keys.map(x => list[x]);
-      } else {
-        index--;
+import R from 'ramda';
+
+export const combinations = function* (list, n) {
+  var keys = [];
+  var index = 0;
+  for (var i = 0; i < n; i++) {
+    keys.push(-1);
+  }
+  while (index >= 0) {
+    if (keys[index] < list.length - (n - index)) {
+      for (var key = keys[index] - index + 1; index < n; index++) {
+        keys[index] = key + index;
       }
+      yield keys.map(x => list[x]);
+    } else {
+      index--;
     }
-  };
+  }
+};
+
+export const permutations = R.compose(R.sequence(R.of), R.flip(R.repeat));
 
 export const pairs = xs => {
   const pair = [];
@@ -24,4 +28,4 @@ export const pairs = xs => {
     }
   }
   return pair;
-}
+};
