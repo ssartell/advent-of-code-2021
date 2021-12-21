62173358c8ff012c6e757cee174767185d4de7a0
Sean Sartell <ssartell@rightpoint.com>
2021-12-21 14:33:06 -0600

    day21

--- day21/part2.js
+++ day21/part2.js
@@ -4,7 +4,7 @@ import { permutations } from '../utils/combinations.js';
 
 const parseInput = R.pipe(R.split('\r\n'), R.map(R.pipe(R.split(':'), R.last, Number)));
 
-const getKey = (a, b, c, d) => `${a}-${b}-${c}-${d}`;
+const getKey = (a, b, c, d) => `${a},${b},${c},${d}`;
 const diracDice = R.memoizeWith(getKey, (p1Pos, p2Pos, p1Score, p2Score) => {
   let wins = [0, 0];
   for(let [d1, d2, d3] of permutations(3, [1,2,3])) {
@@ -13,9 +13,9 @@ const diracDice = R.memoizeWith(getKey, (p1Pos, p2Pos, p1Score, p2Score) => {
     if (newP1Score >= 21) {
       wins[0]++;
     } else {
-      let futureWins = diracDice(p2Pos, newP1Pos, p2Score, newP1Score);
-      wins[0] += futureWins[1];
-      wins[1] += futureWins[0];
+      let [ futureP2Wins, futureP1Wins ] = diracDice(p2Pos, newP1Pos, p2Score, newP1Score);
+      wins[0] += futureP1Wins;
+      wins[1] += futureP2Wins;
     }
   }
   return wins;
